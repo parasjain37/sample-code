db35265512bc46971113808d629c8bd32922ccce
parasjain37 <52083776+parasjain37@users.noreply.github.com>
2020-09-01 20:22:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 <html>
 <body>
 
-<h1>My first PHP page for UNNATI WEBHOOK v2.1</h1>
-  <marquee> Learning WEBHOOKS</marquee>
+<h1>My first PHP page for WEBHOOK </h1>
 <?php
 echo "Hello World! from GIT";
 echo "Hello from version1.1  ";
-echo " Why to Worry ? S2I in openshift";
+echo "I Love Opensource";
 ?>
 
 </body>
